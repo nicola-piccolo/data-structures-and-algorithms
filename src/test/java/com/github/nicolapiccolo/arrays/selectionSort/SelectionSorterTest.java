c75791a1419ca96ace1bafe317a1da21f3f9c128
Nicola <nickhannibal@gmail.com>
2024-05-31 20:28:40 +0900

    Formatted unit tests for selection sort

--- src/test/java/com/github/nicolapiccolo/arrays/selectionSort/SelectionSorterTest.java
+++ src/test/java/com/github/nicolapiccolo/arrays/selectionSort/SelectionSorterTest.java
@@ -13,6 +13,7 @@ public class SelectionSorterTest {
 		sorter.sort(arrayToSort);
 		assertTrue(arrayToSort == null);
 	}
+
 	@Test
 	public void emptyArray_nothingToSort() {
 		Integer[] arrayToSort = new Integer[0];
@@ -20,50 +21,55 @@ public class SelectionSorterTest {
 		sorter.sort(arrayToSort);
 		assertTrue(arrayToSort.length == 0);
 	}
+
 	@Test
 	public void oneElementArray_nothingToSort() {
 		Integer element = 4;
-		Integer[] arrayToSort = new Integer[]{element};
+		Integer[] arrayToSort = new Integer[] { element };
 		SelectionSorter sorter = new SelectionSorter();
 		sorter.sort(arrayToSort);
 		assertTrue(arrayToSort[0] == element);
 	}
+
 	@Test
 	public void twoElementOrderedArray_nothingToSort() {
 		Integer firstElement = 4;
 		Integer secondElement = 40;
-		Integer[] arrayToSort = new Integer[]{firstElement, secondElement};
+		Integer[] arrayToSort = new Integer[] { firstElement, secondElement };
 		SelectionSorter sorter = new SelectionSorter();
 		sorter.sort(arrayToSort);
-		assertArrayEquals(arrayToSort, new Integer[]{firstElement, secondElement});
+		assertArrayEquals(arrayToSort, new Integer[] { firstElement, secondElement });
 	}
+
 	@Test
 	public void twoElementUnorderedArray_sort() {
 		Integer firstElement = 4;
 		Integer secondElement = 40;
-		Integer[] arrayToSort = new Integer[]{secondElement, firstElement};
+		Integer[] arrayToSort = new Integer[] { secondElement, firstElement };
 		SelectionSorter sorter = new SelectionSorter();
 		sorter.sort(arrayToSort);
-		assertArrayEquals(arrayToSort, new Integer[]{firstElement, secondElement});
+		assertArrayEquals(arrayToSort, new Integer[] { firstElement, secondElement });
 	}
+
 	@Test
 	public void threeElementOrderedArray_nothingToSort() {
 		Integer firstElement = 4;
 		Integer secondElement = 40;
 		Integer thirdElement = 400;
-		Integer[] arrayToSort = new Integer[]{firstElement, secondElement, thirdElement};
+		Integer[] arrayToSort = new Integer[] { firstElement, secondElement, thirdElement };
 		SelectionSorter sorter = new SelectionSorter();
 		sorter.sort(arrayToSort);
-		assertArrayEquals(arrayToSort, new Integer[]{firstElement, secondElement, thirdElement});
+		assertArrayEquals(arrayToSort, new Integer[] { firstElement, secondElement, thirdElement });
 	}
+
 	@Test
 	public void threeElementUnorderedArray_sort() {
 		Integer firstElement = 4;
 		Integer secondElement = 40;
 		Integer thirdElement = 400;
-		Integer[] arrayToSort = new Integer[]{thirdElement, secondElement, firstElement};
+		Integer[] arrayToSort = new Integer[] { thirdElement, secondElement, firstElement };
 		SelectionSorter sorter = new SelectionSorter();
 		sorter.sort(arrayToSort);
-		assertArrayEquals(arrayToSort, new Integer[]{firstElement, secondElement, thirdElement});
+		assertArrayEquals(arrayToSort, new Integer[] { firstElement, secondElement, thirdElement });
 	}
 }
