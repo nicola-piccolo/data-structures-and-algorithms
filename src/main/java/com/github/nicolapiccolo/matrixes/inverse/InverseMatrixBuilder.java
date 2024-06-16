4569535e099d4ef63c71b6188262d6c982122572
Nicola <nickhannibal@gmail.com>
2024-06-16 22:40:51 +0900

    Improve inverse matrix builder

--- src/main/java/com/github/nicolapiccolo/matrixes/inverse/InverseMatrixBuilder.java
+++ src/main/java/com/github/nicolapiccolo/matrixes/inverse/InverseMatrixBuilder.java
@@ -22,34 +22,53 @@ public class InverseMatrixBuilder {
 	}
 
 	private void applyJordanGaussEliminationTo(double[][] items) {
-		for (int diagonalIndex = 0; diagonalIndex < items.length; diagonalIndex++) {
+		int rowDimension = items.length;
+		for (int diagonalIndex = 0; diagonalIndex < rowDimension; diagonalIndex++) {
 			if (items[diagonalIndex][diagonalIndex] == 0) {
 				throw new RuntimeException("Cannot calculate matrix inverse!");
 			}
-			for (int rowIndex = 0; rowIndex < items.length; rowIndex++) {
-				if (rowIndex != diagonalIndex) {
-					double ratio = items[rowIndex][diagonalIndex] / items[diagonalIndex][diagonalIndex];
-					for (int columnIndex = 0; columnIndex < 2 * items.length; columnIndex++) {
-						items[rowIndex][columnIndex] -= ratio * items[diagonalIndex][columnIndex];
-					}
+			for (int currentRowIndex = 0; currentRowIndex < rowDimension; currentRowIndex++) {
+				if (currentRowIndex != diagonalIndex) {
+					this.multiplyAndSumToCurrentRow(currentRowIndex, diagonalIndex, items);
 				}
 			}
 		}
 	}
 
+	private void multiplyAndSumToCurrentRow(int currentRowIndex, int diagonalIndex, double[][] items) {
+		double ratio = this.getRatioBetween(currentRowIndex, diagonalIndex, items);
+		this.sumDiagonalRowToCurrentRow(currentRowIndex, diagonalIndex, ratio, items);
+	}
+
+	private double getRatioBetween(int currentRowIndex, int diagonalIndex, double[][] items) {
+		double ratio = items[currentRowIndex][diagonalIndex] / items[diagonalIndex][diagonalIndex];
+		return ratio;
+	}
+
+	private void sumDiagonalRowToCurrentRow(int currentRowIndex, int diagonalIndex, double ratio, double[][] items) {
+		int columnDimension = items.length * 2;
+		for (int columnIndex = 0; columnIndex < columnDimension; columnIndex++) {
+			items[currentRowIndex][columnIndex] -= ratio * items[diagonalIndex][columnIndex];
+		}
+	}
+
 	private void normalizeMatrix(double[][] items) {
-		for (int rowIndex = 0; rowIndex < items.length; rowIndex++) {
-			for (int columnIndex = items.length; columnIndex < 2 * items.length; columnIndex++) {
+		int rowDimension = items.length;
+		int columnDimension = items.length * 2;
+		for (int rowIndex = 0; rowIndex < rowDimension; rowIndex++) {
+			for (int columnIndex = rowDimension; columnIndex < columnDimension; columnIndex++) {
 				items[rowIndex][columnIndex] /= items[rowIndex][rowIndex];
 			}
 		}
 	}
 
 	private ImmutableMatrix buildInverseImmutableMatrixFrom(double[][] items) {
-		double[][] inverse = new double[items.length][items.length];
-		for (int rowIndex = 0; rowIndex < items.length; rowIndex++) {
-			for (int columnIndex = 0; columnIndex < items.length; columnIndex++) {
-				inverse[rowIndex][columnIndex] = items[rowIndex][items.length + columnIndex];
+		int rank = items.length;
+		double[][] inverse = new double[rank][rank];
+		for (int rowIndex = 0; rowIndex < rank; rowIndex++) {
+			for (int columnIndex = 0; columnIndex < rank; columnIndex++) {
+				int augmentedColumnIndex = items.length + columnIndex;
+				inverse[rowIndex][columnIndex] = items[rowIndex][augmentedColumnIndex];
 			}
 		}
 		return new ImmutableMatrix(inverse);
